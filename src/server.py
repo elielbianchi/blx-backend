4511b544c12c1a4a7a78f55886a6ef7e98e68328
Eliel Bianchi <elielbianchi@hotmail.com>
2023-03-13 17:02:09 -0300

    feat: adiciona CORS middleware e rotas put e delete de produtos ao server

--- src/server.py
+++ src/server.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from sqlalchemy.orm import Session
-from src.schemas.schemas import Produto, Usuario
+from src.schemas.schemas import Produto, ProdutoSimples, Usuario
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
 from src.infra.sqlalchemy.repositories.produto import RepositorioProduto
 from src.infra.sqlalchemy.repositories.usuario import RepositorioUsuario
@@ -10,8 +11,18 @@ from src.infra.sqlalchemy.repositories.usuario import RepositorioUsuario
 
 app = FastAPI()
 
-# PRODUTOS
+# CORS
 
+origins = ['http://localhost:3000']
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
+
+
+# PRODUTOS
 
 @app.get("/produtos", response_model=List[Produto])
 def listar_produtos(session: Session = Depends(get_db)):
@@ -25,6 +36,19 @@ def criar_produto(produto: Produto, session: Session = Depends(get_db)):
     return produto_criado
 
 
+@app.put("/produtos/{id}", response_model=ProdutoSimples)
+def atualizar_produto(id: int, produto: Produto, session: Session = Depends(get_db)):
+    RepositorioProduto(session).editar(id, produto)
+    produto.id = id
+    return produto
+
+
+@app.delete("/produtos/{id}")
+def remover_produto(id: int, session: Session = Depends(get_db)):
+    RepositorioProduto(session).remover(id)
+    return
+
+
 # USUARIOS
 
 @app.get("/usuarios", response_model=List[Usuario])
@@ -33,7 +57,7 @@ def listar_usuarios(session: Session = Depends(get_db)):
     return usuarios
 
 
-@app.post("/usuarios", status_code=status.HTTP_201_CREATED, response_model=Usuario)
-def criar_usuario(usuario: Usuario, session: Session = Depends(get_db)):
+@app.post("/signup", status_code=status.HTTP_201_CREATED, response_model=Usuario)
+def signup(usuario: Usuario, session: Session = Depends(get_db)):
     usuario_criado = RepositorioUsuario(session).criar(usuario)
     return usuario_criado
