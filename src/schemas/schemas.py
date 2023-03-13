202bcd7a40e66270a42164a214f69a3f061f9efb
Eliel Bianchi <elielbianchi@hotmail.com>
2023-03-13 17:00:18 -0300

    refactor: refatora repositorio de produto e schemas

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -2,14 +2,33 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 
+class ProdutoSimples(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    preco: float
+    disponivel: bool = False
+
+    class Config:
+        orm_mode = True
+
+
 class Usuario(BaseModel):
     id: Optional[int] = None
     nome: str
     telefone: str
     senha: str
-    # meus_produtos: List['Produto']
+    produtos: List[ProdutoSimples] = []
     # minhas_vendas: List['Pedido']
     # meus_pedidos: List['Pedido']
+
+    class Config:
+        orm_mode = True
+
+
+class UsuarioSimples(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    telefone: str
 
     class Config:
         orm_mode = True
@@ -21,15 +40,15 @@ class Produto(BaseModel):
     detalhes: str
     preco: float
     disponivel: bool = False
-    usuario_id: int
-    usuario: Optional[Usuario]
+    usuario_id: Optional[int]
+    usuario: Optional[UsuarioSimples]
 
     class Config:
         orm_mode = True
 
 
 class Pedido(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
     usuario: Usuario
     produto: Produto
     quantidade: int
