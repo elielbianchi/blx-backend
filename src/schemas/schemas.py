90f4911899583dde71e27d8cbaca05a51913fb64
Eliel Bianchi <elielbianchi@hotmail.com>
2023-03-13 15:02:04 -0300

    refactor: refatora models e schemas

--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -3,20 +3,26 @@ from typing import Optional, List
 
 
 class Usuario(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
     nome: str
     telefone: str
-    meus_produtos: List['Produto']
-    minhas_vendas: List['Pedido']
-    meus_pedidos: List['Pedido']
+    senha: str
+    # meus_produtos: List['Produto']
+    # minhas_vendas: List['Pedido']
+    # meus_pedidos: List['Pedido']
+
+    class Config:
+        orm_mode = True
 
 
 class Produto(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
     nome: str
     detalhes: str
     preco: float
     disponivel: bool = False
+    usuario_id: int
+    usuario: Optional[Usuario]
 
     class Config:
         orm_mode = True
